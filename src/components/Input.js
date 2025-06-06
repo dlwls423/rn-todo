56823043b5e3bee3939173a6d8794e2082b472f2
yejin <leeyejin113@gmail.com>
2025-06-06 14:55:31 +0900

    로그인 화면과 입력

---
+++ src/components/Input.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export const KeyboardTypes = {
+  DEFAULT: 'default',
+  EMAIL: 'email-address',
+};
+
+export const ReturnKeyTypes = {
+  DONE: 'done',
+  NEXT: 'next',
+};
+
+const Input = ({ title, placeholder, ...props }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <TextInput
+        {...props}
+        style={styles.input}
+        placeholder={placeholder ?? title}
+        placeholderTextColor={'#a3a3a3'}
+        autoCapitalize="none"
+        autoCorrect={false}
+        textContentType="none"
+        keyboardAppearance="light"
+      />
+    </View>
+  );
+};
+
+Input.propTypes = {
+  title: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  title: {
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    height: 42,
+  },
+});
+
+export default Input;
